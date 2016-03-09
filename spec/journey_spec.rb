6867b4593962b714909b0c98b8f3d3458a9415d0
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-09 11:45:32 +0000

    refactored

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,31 +4,24 @@ describe Journey do
   subject(:journey) { described_class.new }
   let(:euston) { double :entry_station }
   let(:camden) { double :exit_station }
-
-  it { is_expected.to respond_to(:entry_station) }
-
-  it { is_expected.to respond_to(:exit_station) }
-
-
-
+  before do
+    journey.start_journey(euston)
+    journey.complete_journey(camden)
+  end
   describe "#start_journey" do
     it 'should store entry_station' do
-      journey.start_journey(euston)
       expect(journey.entry_station).to eq euston
     end
   end
 
   describe "#complete_journey" do
     it 'should store exit_station' do
-      journey.complete_journey(camden)
       expect(journey.exit_station).to eq camden
     end
   end
 
   describe "#make_journey" do
     it 'returns a hash of entry_station and exit_station' do
-      journey.start_journey(euston)
-      journey.complete_journey(camden)
       expect(journey.make_journey).to eq ({ entry_station: euston, exit_station: camden })
     end
   end
